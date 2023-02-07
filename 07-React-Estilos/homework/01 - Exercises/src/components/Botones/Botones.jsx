818a6ec94bdb499dbaf0c79cad513c1aacf59719
YoungSan14 <suarezsan14@gmail.com>
2023-02-07 17:07:07 -0300

    test 11/11

--- 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
+++ 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
 
-
-const DivButtons = 'DivButtons'
-const Buttons = 'Buttons'
+const DivButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+const Buttons = styled.button`
+  border-radius: 5px;
+  margin: 10px;
+  padding: 5px;
+  cursor: pointer;
+  &:hover{
+    background: black;
+    color: whitesmoke;
+  }
+`;
 
 export default class Botones extends React.Component {
   render () {
     const { alerts } = this.props
     return (
-      <div>
-        <button onClick={() => window.alert(alerts.m1)}>Módulo 1</button>
-        <button onClick={() => window.alert(alerts.m2)}>Módulo 2</button>
-      </div>
+      <DivButtons>
+        <Buttons onClick={() => window.alert(alerts.m1)}>Módulo 1</Buttons>
+        <Buttons onClick={() => window.alert(alerts.m2)}>Módulo 2</Buttons>
+        {/* <button onClick={() => window.alert(alerts.m1)}>Módulo 1</button>
+        <button onClick={() => window.alert(alerts.m2)}>Módulo 2</button> */}
+      </DivButtons>
     )
   }
 }
